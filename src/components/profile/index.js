c213eb84b53550d327de881cac2c6ab93bb31901
Uchoa <joao.uchoa@atos.net>
2022-01-02 11:02:18 -0300

    Feature: build a new component

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import useGithub from "../../hooks/github-hooks";
-import * as S from "./styled";
+
 
 const Profile = () => {
   const { githubState } = useGithub();
@@ -36,24 +36,7 @@ const Profile = () => {
             </a>
           </S.WrapperUserGeneric>
         </div>
-        <S.WrapperStatusCount>
-          <div>
-            <h4>Followers</h4>
-            <span> {githubState.user.followers}</span>
-          </div>
-          <div>
-            <h4>Followings</h4>
-            <span> {githubState.user.following}</span>
-          </div>
-          <div>
-            <h4>Gists</h4>
-            <span> {githubState.user.public_gists}</span>
-          </div>
-          <div>
-            <h4>Repos</h4>
-            <span> {githubState.user.public_repos}</span>
-          </div>
-        </S.WrapperStatusCount>
+        <StatusCount/>
       </S.WrapperInfoUser>
     </S.Wrapper>
   );
---
+++ src/components/statusCount/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-componentsS';
+
+export const WrapperStatusCount = styled.div`
+  display: flex;
+  align-items: center;
+  div {
+    margin: 8px;
+    text-align: center;
+  }
+`;
